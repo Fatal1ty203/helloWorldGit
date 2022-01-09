92605c97cfa7a61a01505e57fedd2aed4229009c
Fatal1ty203 <85799432+Fatal1ty203@users.noreply.github.com>
2022-01-09 16:56:28 +0300

    console input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Пожалуйста введите фразу для вывода на экран: ");
+        String line = scn.nextLine();
         for (int i = 0; i <5 ; i ++){
-            System.out.println("Hello World");
+            System.out.println(line);
         }
     }
 }
